49c77215c01772b41abd335cdfd476b722e9d8e4
alexander polyakov <alexander.polyakov@breffi.ru>
2016-03-04 17:51:59 +0300

    Init

---
+++ 04_Slider/js/metrica.js
@@ -0,0 +1,6 @@
+var whatManStr = localStorage.getItem('men'),
+    whatGlukoStr = localStorage.getItem('gluko'),
+    whatButton23Str = localStorage.getItem('button23'),
+    whatButton16Str = localStorage.getItem('button16'),
+    whatButton32Str = localStorage.getItem('button32');
+//StoryCLM.CustomEvents.Set('Pacient', whatManStr, function(data){};
